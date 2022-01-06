e51ef6ef5efdcb1250159330a06492852a2353f3
maycon.huanca <maycon.huanca@CLBBRC02RP8BJG8WP.local>
2022-01-06 01:53:47 -0300

    add, list and read notes

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-const notes = require('../course-nodejs/notes');
+//const notes = require('../course-nodejs/notes');
+const chalk = require('chalk');
 const yargs = require('yargs');
 const { hideBin } = require('yargs/helpers')
+const fs = require('fs');
+
 
 yargs(hideBin(process.argv))
     .command({
         command: 'add',
         describe: 'adding command',
-        handler: () => {
-            console.log("adding notes")
+        builder: {
+            title: {
+                describe: "Note title",
+                demandOption: true,
+                type: 'string'
+            },
+            body: {
+                describe: "content o note",
+                demandOption: true,
+                type: 'string'
+            }
+        },
+
+        handler: (argv) => {
+
+            fs.writeFileSync("./notes/" + argv.title + ".json", JSON.stringify({ title: argv.title, body: argv.body }));
+            console.log(chalk.green("new file created: " + argv.title));
+
         }
     })
     .command({
@@ -21,14 +40,42 @@ yargs(hideBin(process.argv))
         command: 'list',
         describe: 'listing command',
         handler: () => {
-            console.log("listing notes")
+
+            fs.readdir("./notes", (err, notes) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                notes.forEach((note, index) => console.log(chalk.green(index + " - " + note)));
+            });
+
         }
     })
     .command({
         command: 'read',
         describe: 'reading command',
-        handler: () => {
-            console.log("reading notes")
+        builder: {
+            file: {
+                describe: "the file name",
+                demandOption: true,
+                type: "string"
+            }
+        },
+        handler: (argv) => {
+
+            fs.readFile(__dirname + "/notes/" + argv.file, { encoding: 'utf-8', flag: 'r' }, (error, data) => {
+                if (error) {
+                    console.log(chalk.red("something went wrong"));
+                    return;
+                }
+
+                let note = JSON.parse(data);
+
+                for (let value in note) {
+                    console.log(chalk.green.bold(value) + "\n   " + chalk.green(note[value]));
+                }
+
+            });
         }
     }).parse();
 
